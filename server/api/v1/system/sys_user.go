bfdc7dfaec170dbedc0470fbb1c4e0e99b9c0553
jasvtfvan <jasvtfvan@163.com>
2024-04-02 17:52:32 +0800

    操作频繁不再记录

--- server/api/v1/system/sys_user.go
+++ server/api/v1/system/sys_user.go
@@ -101,8 +101,7 @@ func (u *UserApi) Captcha(c *gin.Context) {
 	}
 
 	if buildCountCount >= openCaptchaBuildCountMax {
-		captchaBuildCountStore.AddCount(key) // 生成验证码次数+1
-		response.Fail(nil, "操作太频繁，过一阵再来尝试。也可以联系管理员", c)
+		response.Fail(nil, "操作太频繁，过一阵再来尝试。", c)
 		return
 	}
 
@@ -151,8 +150,7 @@ func (u *UserApi) Login(c *gin.Context) {
 	}
 
 	if count >= openCaptchaMax { // 超过最大次数，锁定
-		captchaLoginCountStore.AddCount(key) // 验证码次数+1
-		response.Fail(nil, "错误太过频繁，过一阵再来尝试。也可以联系管理员", c)
+		response.Fail(nil, "错误太过频繁，过一阵再来尝试。", c)
 		return
 	}
 
